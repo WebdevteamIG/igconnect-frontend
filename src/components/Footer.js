2c6f03812b6d5323703225f8505057386e1485c8
unknown <abhinavs2709@gmail.com>
2020-11-16 23:07:49 +0530

    added scrolltoTop & fix oic page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,94 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import '../App.css';
 import igrect from './images/Ig+logo+rectangular.png';
 import c4ii from './images/C4II.png';
-import { Link } from 'react-router-dom';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div>
-        {/* <!-- Footer --> */}
-        <footer className="page-footer font-small mdb-color lighten-3 pt-4">
-          {/* <!-- Footer Links --> */}
-          <div className="container text-center text-md-left">
-            {/* <!-- Grid row --> */}
-            <div className="row">
-              {/* <!-- Grid column --> */}
-              <div className="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
-                <img
-                  src={igrect}
-                  alt="Ig+logo+rectangular.png"
-                  width="250px"
-                  height="80px"
-                  className=""
-                />
-                <img
-                  src={c4ii}
-                  alt="Ig+logo+rectangular.png"
-                  width="200px"
-                  height="110px"
-                  className=" mt-3"
-                />
-              </div>
-              {/* <!-- Grid column --> */}
+const Footer = () => {
 
-              <hr className="clearfix w-100 d-md-none" />
+  const [showScroll, setShowScroll] = useState(false)
 
-              {/* <!-- Grid column --> */}
-              <div className="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false)
+    }
+  };
 
-                <ul className="list-unstyled">
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  window.addEventListener('scroll', checkScrollTop)
+
+  return (
+    <div>
+      {/* <!-- Footer --> */}
+      <footer className="page-footer font-small mdb-color lighten-3 pt-4">
+        {/* <!-- Footer Links --> */}
+        <div className="container text-center text-md-left">
+          {/* <!-- Grid row --> */}
+          <div className="row">
+            {/* <!-- Grid column --> */}
+            <div className="col-md-4 col-lg-4 mr-auto my-md-4 my-0 mt-4 mb-1">
+              <img
+                src={igrect}
+                alt="Ig+logo+rectangular.png"
+                width="250px"
+                height="80px"
+                className=""
+              />
+              <img
+                src={c4ii}
+                alt="Ig+logo+rectangular.png"
+                width="250px"
+                height="110px"
+                className=" mt-3"
+              />
+            </div>
+
+            {/* <!-- Grid column --> */}
+            <hr className="clearfix w-100 d-md-none" />
+
+            {/* <!-- Grid column --> */}
+            <div className="col-md-4 col-lg-4 mx-auto my-md-4 my-0 mt-4 mb-1">
+              {/* <!-- Contact details --> */}
+              <h2 className="font-weight-bold text-uppercase mb-4">
+                Contact Us
+  </h2>
+
+              <ul className="list-unstyled">
+                <li>
+                  <p className="para-contact-us">
+                    <i className="fas fa-home mr-3 "></i>
+        NIT Warangal, Telangana
+      </p>
+                </li>
+                <li>
+                  <p className="para-contact-us">
+                    <i className="fas fa-envelope mr-3"></i>{" "}
+        ig-nitw@student.nitw.ac.in
+      </p>
+                </li>
+                <li>
+                  <p className="para-contact-us">
+                    <i className="fas fa-phone mr-3"></i> +91 79 8786 8575
+      </p>
+                </li>
+                {/* <li>
+      <p>
+        <i className="fas fa-print mr-3"></i> +9 999 888 77 74</p>
+    </li> */}
+              </ul>
+            </div>
+
+
+            {/* <!-- Grid column --> */}
+            <hr className="clearfix w-100 d-md-none" />
+            {/* <!-- Grid column --> */}
+            <div className="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1 text-center">
+
+              {/* <ul className="list-unstyled">
                   <li>
                     <h5 className="font-weight-bold text-uppercase mb-4 ">
                       <Link to={`${process.env.PUBLIC_URL}/about`} className="footer-links">About</Link></h5>
@@ -48,106 +99,68 @@ export default class Footer extends Component {
                       <Link to={`${process.env.PUBLIC_URL}/events`} className="footer-links">Events </Link></h5>
 
                   </li>
-                  {/* <!-- Links --> */}
+                </ul> */}
 
-                  {/* <ul className="list-unstyled">
-                    <li><a
-                      href="https://www.facebook.com/TheInnovationGarage"
-                      className="icon-button"
-                    >
-                      <i className="fab fa-facebook-f"></i>
-                    </a><br /><br /></li>
-                    <li><a
-                      href="https://www.instagram.com/ig_nitw/"
-                      className="icon-button"
-                    >
-                      <i className="fab fa-instagram"></i>
-                    </a><br /><br /></li>
-                    <li><a
-                      href="https://www.instagram.com/ig_nitw/"
-                      className="icon-button"
-                      id="last-social-icon"
-                    >
-                      <i className="fab fa-linkedin-in "></i>
-                    </a></li> */}
-                </ul>
-              </div>
+              {/* <!-- Social Links --> */}
 
-              {/* <!-- Grid column --> */}
-              {/* blog start */}
-              {/* <hr className="clearfix w-100 d-md-none" />
-              <div className="col-md-2 col-lg-2 text-left  my-4">
-                <h5 className="font-weight-bold text-uppercase mb-4 ">BLOG</h5>
-                <ul className="list-unstyled">
-                  <li>
-                    <p >LATEST</p>
-                  </li>
-                  <li>
-                    <p >TECH</p>
-                  </li>
-                  <li>
-                    <p >DESIGN</p>
-                  </li>
-                  <li>
-                    <p >REPORT</p>
-                  </li>
-                </ul>
-              </div> */}
-              {/* blog end */}
+              <ul className="list-unstyled">
+                <li><a
+                  href="https://www.facebook.com/TheInnovationGarage"
+                  className="icon-button"
+                >
+                  <i className="fab fa-facebook-f"></i>
+                </a><br /><br /></li>
+                <li><a
+                  href="https://www.instagram.com/ig.nitw/"
+                  className="icon-button"
+                >
+                  <i className="fab fa-instagram"></i>
+                </a><br /><br /></li>
+                <li><a
+                  href="https://www.linkedin.com/company/ignitw"
+                  className="icon-button"
+                // id="last-social-icon"
+                >
+                  <i className="fab fa-linkedin-in "></i>
+                </a></li>
+                <li><a
+                  href="https://linktr.ee/ig.nitw"
+                  className="icon-button"
+                  id="last-social-icon"
+                >
+                  <i className="fas fa-link fab"></i>
+                </a></li>
+              </ul>
 
-              {/* <!-- Grid column --> */}
-
-              <hr className="clearfix w-100 d-md-none" />
-
-              {/* <!-- Grid column --> */}
-              <div className="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-                {/* <!-- Contact details --> */}
-                <h5 className="font-weight-bold text-uppercase mb-4">
-                  Contact Us
-                </h5>
-
-                <ul className="list-unstyled">
-                  <li>
-                    <p className="">
-                      <i className="fas fa-home mr-3 "></i>
-                      NIT Warangal, Telangana
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      <i className="fas fa-envelope mr-3"></i>{" "}
-                      ig-nitw@student.nitw.ac.in
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      <i className="fas fa-phone mr-3"></i> +91 79 8786 8575
-                    </p>
-                  </li>
-                  {/* <li>
-                    <p>
-                      <i className="fas fa-print mr-3"></i> +9 999 888 77 74</p>
-                  </li> */}
-                </ul>
-              </div>
             </div>
-            {/* <!-- Grid row --> */}
+
+            <hr className="clearfix w-100 d-md-none" />
+            <div className="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1 scrollTopDiv">
+              {/* Scroll to top button */}
+              <button onClick={scrollTop} className="scrollTop"
+              // style={{ height: 40, display: showScroll ? 'flex' : 'none' }}
+              >
+                <i className="fas fa-arrow-circle-up scrollTopIcon"></i>
+              </button>
+            </div>
           </div>
-          {/* <!-- Footer Links --> */}
-          <hr />
-          {/* <!-- Copyright --> */}
-          <div className="footer-copyright text-center py-3 font-weight-bold">
-            © 2020 Copyright: InnovationGarage
+          {/* <!-- Grid row --> */}
+        </div>
+        {/* <!-- Footer Links --> */}
+        <hr />
+        {/* <!-- Copyright --> */}
+        <div className="footer-copyright text-center py-3 font-weight-bold">
+          © 2020 Copyright: InnovationGarage, NIT Warangal
           </div>
+        {/* <!-- Copyright --> */}
+        <br />
+        <br />
+      </footer>
+      {/* <!-- Footer --></div> */}
 
-          {/* <!-- Copyright --> */}
-          <br />
-          <br />
 
-
-        </footer>
-        {/* <!-- Footer --></div> */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Footer;
--- src/components/OicPage/OicPage.js
+++ src/components/OicPage/OicPage.js
@@ -21,9 +21,16 @@ export default class OicPage extends Component {
 
             <h1 className="heading1 ml-5">OPEN INNOVATION CHALLENGE</h1>
             <div className="mt-4  pl-2 text-left register-btn">
-              <a href="https://ignitw.typeform.com/to/Ihz9RLmH" type="button" className="mybtn "
-                style={{ lineHeight: "25px" }}>Register for All-India Open Innovation Challenge<i className="fas fa-angle-double-right ml-2 fas-icon"></i></a>
-              <center><Link to={`${process.env.PUBLIC_URL}/events/oic/ideasubmission`} className="mybtn">Idea Submission</Link></center>
+              <p className="mx-auto mt-1" id="mainHeaderDescription" > In line with Atma Nirbhar Abhiyan, CII, NITW is conducting an Open Innocation Challenge for students and startups for developing apps and toys for children to help in their learning.
+              </p>
+
+              <a href="https://ignitw.typeform.com/to/Ihz9RLmH" type="button" className="mybtn"
+                style={{ lineHeight: "25px" }}>REGISTER FOR ALL-INDIA OPEN INNOVATION CHALLENGE
+                {/* <i className="fas fa-angle-double-right ml-2 fas-icon"></i> */}
+              </a>
+              <Link to={`${process.env.PUBLIC_URL}/events/oic/ideasubmission`}
+                className="mybtn btn_Idea_Submission mt-1">IDEA SUBMISSION</Link>
+
               {/* <button className="mybtn mt-3 "> VIEW EVENTS<i className="fas fa-angle-double-right ml-2"></i></button> */}
             </div >
 
@@ -34,15 +41,16 @@ export default class OicPage extends Component {
           </div>
         </div>
         <div className="text-center">
-          <p className="mx-auto" id="mainHeaderDescription" > In line with Atma Nirbhar Abhiyan, CII, NITW is conducting on Open Innocation Challenge for students and startups for developing apps and toys for children to help in their learning.
-         </p>
+
           {/* Audio tag */}
-          <div >
+          <div className="text-center">
             <img src={directorpic} alt="Director" width="20%" height="30%" />
+
+          </div>
+          <div>
             <p className="d-inline audioLabel" > A message from our esteemed Director:</p>
             <audio className="d-inline ml-4 mt-2 audioLabel" ref="audio_tag" src={audio} controls type="audio/mpeg" autoPlay={false} />
           </div>
-
           {/* Audio tag end*/}
         </div>
 
