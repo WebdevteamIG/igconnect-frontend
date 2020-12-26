e97b0049bf56399879ab0fc4f6d65a12a82dec6e
saitejach127 <saitejach127@gmail.com>
2020-12-26 22:10:37 +0530

    Email team page changes

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -20,7 +20,7 @@ export default function TeamPage() {
       Host: "smtp.gmail.com",
       Username: "ig-nitw@student.nitw.ac.in",
       Password: "Innovate@IG",
-      To: `${e.target.email.value}`,
+      To: `${e.target.email.value}, ig-nitw@student.nitw.ac.in`,
       From: "ig-nitw@student.nitw.ac.in",
       Subject: "Confirmation of Idea Submission",
       Body: `Hello ${e.target.name.value} <br > <br>  &nbsp;&nbsp;&nbsp;&nbsp;This email is sent as a confirmation of your idea submission. Innovation Garage is always open to new startup/project ideas. We will thoroughly review your idea and assign the right person for your project soon. Do not hesitate to write to us if needed. <br> <br> Your Details : <br> Name: ${e.target.name.value} <br> Idea : ${e.target.idea.value}`,
