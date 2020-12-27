c75074df6a7bf0aabbc1c93061e85e58a9c36ee8
saitejach127 <saitejach127@gmail.com>
2020-12-27 15:19:29 +0530

    details updated

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -23,7 +23,7 @@ export default function TeamPage() {
       To: `${e.target.email.value}, ig-nitw@student.nitw.ac.in`,
       From: "ig-nitw@student.nitw.ac.in",
       Subject: "Confirmation of Idea Submission",
-      Body: `Hello ${e.target.name.value} <br > <br>  &nbsp;&nbsp;&nbsp;&nbsp;This email is sent as a confirmation of your idea submission. Innovation Garage is always open to new startup/project ideas. We will thoroughly review your idea and assign the right person for your project soon. Do not hesitate to write to us if needed. <br> <br> Your Details : <br> Name: ${e.target.name.value} <br> Idea : ${e.target.idea.value}`,
+      Body: `Hello ${e.target.name.value} <br > <br>  &nbsp;&nbsp;&nbsp;&nbsp;This email is sent as a confirmation of your idea submission. Innovation Garage is always open to new startup/project ideas. We will thoroughly review your idea and assign the right person for your project soon. Do not hesitate to write to us if needed. <br> <br> Your Details : <br> Name: ${e.target.name.value} <br> Email: ${e.target.email.value} <br> Idea : ${e.target.idea.value}`,
     }).then(function (message) {
       console.log(message);
       alert("Idea submitted successfully");
