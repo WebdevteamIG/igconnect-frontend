540ea30a02169cbbaf988aa164a45e5afff9e3b5
cd-Ishita <61752983+cd-Ishita@users.noreply.github.com>
2021-06-19 19:38:49 +0530

    design team added

--- src/components/TeamPage/Design.js
+++ src/components/TeamPage/Design.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 import { Box, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-// import TeamMemberCard from './TeamMemberCard';
-// import nitin from './photos/nitin.jpg';
+import TeamMemberCard from './TeamMemberCard';
+import shriya from './photos/shriya.jpg';
+import chahat from './photos/chahat.jpg';
+import thanish from './photos/thanish.jpg';
+import sreeharsha from './photos/sreeharsha.jpeg';
+import srashti from './photos/srashti.jpg';
+import ankit from './photos/ankit.jpeg';
 
 export default function DesignTeam(props){
 const teamName = "Design Team"
@@ -22,7 +27,12 @@ const teamName = "Design Team"
             <Grid item xs={12}>
               <Box p={1}> 
                 <Grid container spacing={2}>
-                  {/* <Grid ><TeamMemberCard imageURL={nitin} name="Nitin Bhatia" linkedinURL="https://in.linkedin.com/in/aditya-nittala-87b20b17a" githubURL="" email="tasksfornitin@gmail.com" /></Grid> */}
+                  { <Grid ><TeamMemberCard imageURL={ankit} name="Ankit Akkiredi" linkedinURL="https://www.linkedin.com/in/ankit-kumar-akkiredi-975374189/" githubURL="" email="ankit.k.akkiredi2001@gmail.com" /></Grid> }
+                  { <Grid ><TeamMemberCard imageURL={shriya} name="Shriyadita Nath" linkedinURL="NA" githubURL="" email="shriyadita.nath01@gmail.com" /></Grid> }
+                  { <Grid ><TeamMemberCard imageURL={chahat} name="Chahat" linkedinURL="www.linkedin.com/in/chahatl" githubURL="https://github.com/ChahatL" email="chahatl3003@gmail.com" /></Grid> }
+                  { <Grid ><TeamMemberCard imageURL={thanish} name="Thanish Nasir" linkedinURL="https://www.linkedin.com/in/thanish-ahammed-586660141/" githubURL="https://github.com/TNA3" email="thanish@outlook.com" /></Grid> }
+                  { <Grid ><TeamMemberCard imageURL={sreeharsha} name="A.Sai Sreeharsha" linkedinURL="" githubURL="" email="ss832073@student.nitw.ac.in" /></Grid> }
+                  { <Grid ><TeamMemberCard imageURL={srashti} name="Srashti Yadav" linkedinURL="" githubURL="" email="sy862037@student.nitw.ac.in " /></Grid> }
                 </Grid>
                 </Box>
             </Grid>
