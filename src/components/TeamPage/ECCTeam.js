e0d8295d42d96059f67677aeedef3a410c77379d
cd-Ishita <61752983+cd-Ishita@users.noreply.github.com>
2021-06-21 18:58:29 +0530

    added more people

--- src/components/TeamPage/ECCTeam.js
+++ src/components/TeamPage/ECCTeam.js
@@ -9,6 +9,7 @@ import venkatesh from './photos/venkatesh.png';
 import akshat from './photos/akshat.jpeg';
 import lakshayapriya from './photos/lakshayapriya.jpeg';
 import satwik from './photos/satwik.jpg';
+import keerthi from './photos/keerthi.jpeg'
 
 export default function ECCTeam(props){
  const teamName = "ECC Team"
@@ -33,7 +34,7 @@ export default function ECCTeam(props){
                   <Grid ><TeamMemberCard imageURL={akshat} name="Akshat Rai Laddha" linkedinURL="https://www.linkedin.com/in/akshat-rai-laddha-8905a0189" githubURL="" email="ar832035@student.nitw.ac.in" /></Grid>
                   <Grid ><TeamMemberCard imageURL={lakshayapriya} name="S.S. Lakshayapriya" linkedinURL="https://www.linkedin.com/in/lakshayapriya-s-s-432367204" githubURL="" email="laksha_951977@student.nitw.ac.in" /></Grid>
                   <Grid ><TeamMemberCard imageURL={satwik} name="Yagna Satwik" linkedinURL="https://www.linkedin.com/in/yagna-satwik-59b31b214/" githubURL="" email="ys972073@student.nitw.ac.in" /></Grid>
-                 
+                  <Grid ><TeamMemberCard imageURL={keerthi} name="Mikkilineni Venkata Sai Keerthi" linkedinURL="https://www.linkedin.com/in/keerthi-m-71b4a019b" githubURL="" email="mv932016@student.nitw.ac.in" /></Grid>
                  </Grid>
                 </Box>
             </Grid>
--- src/components/TeamPage/PRTeam.js
+++ src/components/TeamPage/PRTeam.js
@@ -10,6 +10,7 @@ import supriya from './photos/supriya.jpeg'
 import shubham from './photos/shubham.jpeg'
 import paila from './photos/paila.jpg'
 import nishanth from './photos/nishanth.jpg'
+import aditya_joshi from './photos/aditya_joshi.jpeg'
 export default function PRTeam(props){
   const teamName = "PR Team"
     return (
@@ -31,6 +32,7 @@ export default function PRTeam(props){
                 <Grid ><TeamMemberCard imageURL={nishanth} name="Nishanth Preetham" linkedinURL="https://www.linkedin.com/in/nishanth-preetham-76a1621b1" githubURL="" email="ng821831@student.nitw.ac.in" /></Grid>  
                 <Grid ><TeamMemberCard imageURL={pavan} name="Chava Pavan Kumar" linkedinURL="http://www.linkedin.com/in/chava-pavan-kumar-0593211b7" githubURL="" email="chava_961918@student.nitw.ac.in" /></Grid>
                 <Grid ><TeamMemberCard imageURL={tanzeela} name="Tanzeela Inam" linkedinURL="https://www.linkedin.com/in/tanzeela-inam-0b48961b9/" githubURL="" email="inam_831984@student.nitw.ac.in" /></Grid>
+                <Grid ><TeamMemberCard imageURL={aditya_joshi} name="Aditya Joshi" linkedinURL="" githubURL="" email="joshi_851975@student.nitw.ac.in" /></Grid>
                 <Grid ><TeamMemberCard imageURL={supriya} name="Supriya gude" linkedinURL="" githubURL="" email="supriyagude24@gmail.com" /></Grid>
                 <Grid ><TeamMemberCard imageURL={shubham} name="Shubham Bapna" linkedinURL="https://www.linkedin.com/in/shubham-bapna-04109b1b3" githubURL="" email="bapna_871975@student.nitw.ac.in" /></Grid>
                 <Grid ><TeamMemberCard imageURL={paila} name="Paila Laxmi Priya" linkedinURL="https://www.linkedin.com/in/laxmi-priya-paila-879964213" githubURL="" email="pl832049@student.nitw.ac.in" /></Grid>
