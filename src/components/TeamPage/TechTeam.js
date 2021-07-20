f2b8b6c58d3d6c97feea1931a83d621c37ea7c9f
Divya Soni <divya.rajeshsoni@gmail.com>
2021-07-20 23:27:41 +0400

    Added profile to teams page

--- src/components/TeamPage/TechTeam.js
+++ src/components/TeamPage/TechTeam.js
@@ -7,6 +7,7 @@ import bhagya from './photos/bhagya.jpg';
 import somal from './photos/somal.png';
 import fateen from './photos/fateen.jpeg';
 import ishitha from './photos/ishitha.jpg';
+import divya from './photos/divya.jpg';
 
 export default function TechTeam(props){
   const teamName = "Technical Team"
@@ -28,7 +29,8 @@ export default function TechTeam(props){
                   <Grid ><TeamMemberCard imageURL={bhagya} name="Bhagya Trivedi" linkedinURL="https://www.linkedin.com/in/bhagya-trivedi-5753aa114/" githubURL="https://github.com/Bhagyatrivedi27" email="bhagyatrivedi20273@gmail.com"/></Grid>
                   <Grid ><TeamMemberCard imageURL={somal} name="Somal Chakraborty" linkedinURL="https://www.linkedin.com/in/somal-chakraborty-8752a6189/" githubURL="https://github.com/SomalRudra" email="sc941871@gmail.com" /></Grid>
                   <Grid ><TeamMemberCard imageURL={fateen} name="Fateen Ahmed" linkedinURL="https://www.linkedin.com/in/fateen-ahmed" githubURL="" email="fateen099@outlook.com"/></Grid>
-                  <Grid ><TeamMemberCard imageURL={ishitha} name="Ishita Gupta" linkedinURL="" githubURL="https://github.com/cd-Ishita" email="ishitagupta.hyd@gmail.com"/></Grid> 
+                  <Grid ><TeamMemberCard imageURL={ishitha} name="Ishita Gupta" linkedinURL="" githubURL="https://github.com/cd-Ishita" email="ishitagupta.hyd@gmail.com"/></Grid>
+                  <Grid ><TeamMemberCard imageURL={divya} name="Divya Soni" linkedinURL="https://www.linkedin.com/in/divya-soni14/" githubURL="https://github.com/divya-soni-14" email="divya.rajeshsoni@gmail.com"/></Grid> 
                 </Grid>
                 </Box>
             </Grid>
